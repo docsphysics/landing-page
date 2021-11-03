f62e54ff5688c339debbc9bb1f2d379dbe923582
docsphysics <91302700+docsphysics@users.noreply.github.com>
2021-11-03 11:42:24 +0800

    "Changes colour of list element upon click of button"

--- script/app.js
+++ script/app.js
@@ -1,37 +1,34 @@
 /* Object for name section etc in Unordered list */
 const ulObj = {
-        sectID: ['section1', 'section2', 'section3', 'section4', 'section5'],
         navName: ['Section 1', 'Section 2', 'Section 3', 'Section 4', 'Section 5'],
-        navID: ['section1-link', 'section2-link', 'section3-link', 'section4-link', 'section5-link'],
         sectNum: 5
 }
 
-const elmt = document.getElementById('header');
 
 /* Create UL using JavaScript for each section on page */
-function createUl(elmt, ulObj) {
+function createUl(ulObj) {
     let li, a, strng;
+    const elmt = document.getElementById('header');
     const ul = document.createElement('ul');
     ul.id = "header-list";
     elmt.appendChild(ul);     // Appended ul to header
 
-    for (let i = 0; i<ulObj.sectNum; i++){
-        li = document.createElement('li'); // Create a new list item
-        li.className = 'listd';
-        a = document.createElement('a'); // Create a link
+    let sectn = document.getElementsByClassName('sectn');
 
-        a.strng = ulObj.sectID[i]; //Section ID
-        a.id = ulObj.navID[i]; // Add an id tag
-        a.innerHTML = ulObj.navName[i]; // Text on navigation link
+
+    for (let i = 0; i<ulObj.sectNum; i++){
+        li = document.createElement('li');  // Create a new list item
+        li.className = 'li-inactive';             // Add class name to li
+        a = document.createElement('a');    // Create a link
+        a.innerHTML = ulObj.navName[i];     // Text on navigation link
 
         /* Click link and scroll to section smoothly */
         a.addEventListener('click',
         function(){
-            scrollFunction(this);
+            scrollFunction();
         });
-        function scrollFunction(item){
-            console.log(item.strng);
-            document.getElementById(item.strng).scrollIntoView({behavior:"smooth"});
+        function scrollFunction(){
+            document.getElementById(sectn[i].id).scrollIntoView({behavior:"smooth"});
         }
 
         li.appendChild(a);
@@ -39,12 +36,14 @@ function createUl(elmt, ulObj) {
     };
 };
 
-createUl(elmt, ulObj); // Call function
+createUl(ulObj); // Call function
+
+
 
 
 function myFunction() {
     let header = document.getElementById('header-list');
-    let links = header.getElementsByClassName('listd');
+    let links = header.getElementsByClassName('li-inactive');
     let sectn = document.getElementsByClassName('sectn');
 
     let ymin = 1000;
@@ -52,6 +51,9 @@ function myFunction() {
     let y;
 
     for (let i = 0; i<sectn.length; i++){
+        if (links[i].className !== 'li-inactive'){
+            links[i].classList.remove('li-active');
+        }
         y = Math.abs(sectn[i].getBoundingClientRect().top);
         if (y<ymin) {
             ymin = y;
@@ -59,30 +61,6 @@ function myFunction() {
         }
     }
 
-    links[imin].classList.toggle("liontop");
+    links[imin].classList.add('li-active');
 
 }
-
-
-/*
-function myFunction(ulObj) {
-    let rect;
-    let ymin = 1000;
-    let imin = 0;
-    const y = [];
-    for (let i = 0; i<ulObj.sectNum; i++){
-        let div = document.getElementById(ulObj.sectID[i]);
-        rect=div.getBoundingClientRect();
-        y[i] = Math.abs(rect.top);
-        if (y[i]<ymin) {
-            ymin = y[i];
-            imin = i;
-        }
-    }
-
-    document.getElementById(ulObj.navID[imin]).classList.toggle("liOnTop");
-
-    console.log(ymin);
-    console.log(imin);
-}
-*/
