a984bc8cda5e27a721dc74b289c24695a779d2cd
docsphysics <91302700+docsphysics@users.noreply.github.com>
2021-11-04 17:30:06 +0800

    "Working file. @media queries to come."

--- script/app.js
+++ script/app.js
@@ -1,6 +1,6 @@
 /* Object for name section etc in Unordered list */
 const ulObj = {
-        navName: ['Section 1', 'Section 2', 'Section 3', 'Section 4', 'Section 5'],
+        navName: ["Section 1", "Section 2", "Section 3", "Section 4", "Section 5"],
         sectNum: 5
 }
 
@@ -8,22 +8,21 @@ const ulObj = {
 /* Create UL using JavaScript for each section on page */
 function createUl(ulObj) {
     let li, a, strng;
-    const elmt = document.getElementById('header');
-    const ul = document.createElement('ul');
+    const elmt = document.getElementById("header");
+    const ul = document.createElement("ul");
     ul.id = "header-list";
     elmt.appendChild(ul);     // Appended ul to header
 
-    let sectn = document.getElementsByClassName('sectn');
-
+    let sectn = document.getElementsByClassName("sectn");
 
     for (let i = 0; i<ulObj.sectNum; i++){
-        li = document.createElement('li');  // Create a new list item
-        li.className = 'li-inactive';             // Add class name to li
-        a = document.createElement('a');    // Create a link
+        li = document.createElement("li");  // Create a new list item
+        li.className = "li-inactive";       // Add class name to li
+        a = document.createElement("a");    // Create a link
         a.innerHTML = ulObj.navName[i];     // Text on navigation link
 
         /* Click link and scroll to section smoothly */
-        a.addEventListener('click',
+        a.addEventListener("click",
         function(){
             scrollFunction();
         });
@@ -38,29 +37,70 @@ function createUl(ulObj) {
 
 createUl(ulObj); // Call function
 
+/* Highlight button when section at top of screen */
+window.addEventListener("scroll", scrollFunction, false);
 
-
-
-function myFunction() {
-    let header = document.getElementById('header-list');
-    let links = header.getElementsByClassName('li-inactive');
-    let sectn = document.getElementsByClassName('sectn');
+function scrollFunction() {
+    let header = document.getElementById("header-list");
+    let links = header.getElementsByClassName("li-inactive");
+    let sectn = document.getElementsByClassName("sectn");
 
     let ymin = 1000;
     let imin = 0;
     let y;
 
     for (let i = 0; i<sectn.length; i++){
-        if (links[i].className !== 'li-inactive'){
-            links[i].classList.remove('li-active');
+        if (links[i].className !== "li-inactive"){
+            links[i].classList.remove("li-active");
         }
-        y = Math.abs(sectn[i].getBoundingClientRect().top);
+        y = Math.abs(sectn[i].getBoundingClientRect().top+60);
         if (y<ymin) {
             ymin = y;
             imin = i;
         }
     }
-
-    links[imin].classList.add('li-active');
-
+    links[imin].classList.add("li-active");
 }
+
+
+/* Collapse section - onclick*/
+function collapseSection(){
+    let btn = document.getElementsByClassName("btn");
+    let content = document.getElementsByClassName("content");
+
+    for (let i = 0; i<btn.length; i++){
+        btn[i].addEventListener("click",
+        function(){
+            collapseFunction();
+        });
+        function collapseFunction(){
+            if (content[i].style.display === "none") {
+                content[i].style.display = "block";
+                btn[i].innerHTML = "&#8211";
+          } else {
+            content[i].style.display = "none";
+            btn[i].innerHTML = "&#43";
+          }
+        }
+    };
+}
+
+collapseSection();
+
+
+/* Collapse Nav bar while not scrolling
+   Leave Nav bar ontop when at top of page
+   Use setInterval to delay collapse */
+let hdrID = document.getElementById("header");
+let didScroll, didMouse;
+hdrID.onmouseenter = function(){didMouse = true};
+hdrID.onmouseleave = function(){didMouse = false};
+window.onscroll = function(){didScroll = true};
+setInterval(function(){
+    if ( didScroll || didMouse || document.body.scrollTop < 100 ) {
+        hdrID.style.opacity = "1";
+        didScroll = false;
+    } else {
+        hdrID.style.opacity = "0";
+    }
+}, 1000);
